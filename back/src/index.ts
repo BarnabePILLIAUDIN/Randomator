70c5ea776cb2a389712412bebb97e8f0aa064074
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2025-10-16 13:01:33 +0200

    chore: eslint + prettier back

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,52 +1,66 @@
-import { serve } from '@hono/node-server'
-import { Hono, type Context } from 'hono'
-import { cors } from 'hono/cors'
+import { serve } from "@hono/node-server"
+import { type Context, Hono } from "hono"
+import { cors } from "hono/cors"
 
 const DEFAULT_MIN = 0
 const DEFAULT_MAX = 100
 const DEFAULT_LENGTH = 5
+const RADIX=10
 const MISSING_CORS_ERROR = "Missing env var for cors"
 const RANDOM_ROUTES = "/random"
 
-const generateRandom = (min: number, max: number) => Math.ceil(min + Math.random() * (max - min))
+const generateRandom = (min: number, max: number) =>
+  Math.ceil(min + Math.random() * (max - min))
 const extractMinAndMax = (ctx: Context) => {
-  const inputMin = Number.parseInt(ctx.req.query("min") ?? "") 
-  const inputMax = Number.parseInt(ctx.req.query("max") ?? "")
+  const inputMin = Number.parseInt(ctx.req.query("min") ?? "",RADIX)
+  const inputMax = Number.parseInt(ctx.req.query("max") ?? "",RADIX)
 
-  const  min  = !Number.isNaN(inputMin) ? inputMin : DEFAULT_MIN 
+  const min = !Number.isNaN(inputMin) ? inputMin : DEFAULT_MIN
   const max = !Number.isNaN(inputMax) ? inputMax : DEFAULT_MAX
-  
-  return {min,max}
 
+  return { min, max }
 }
 
 const app = new Hono({})
 
-const corsOrigin = process.env.CORS ?? (()=>{throw new Error(MISSING_CORS_ERROR)})()
+const corsOrigin =
+  process.env.CORS ??
+  (() => {
+    throw new Error(MISSING_CORS_ERROR)
+  })()
 
-app.use('*', cors({
-  origin: corsOrigin
-}))
+app.use(
+  "*",
+  cors({
+    origin: corsOrigin,
+  }),
+)
 
 app.get(RANDOM_ROUTES, (ctx) => {
-  const {min, max} = extractMinAndMax(ctx)
+  const { min, max } = extractMinAndMax(ctx)
   const number = generateRandom(min, max)
 
-  return ctx.json({success: true, result: number})
+  return ctx.json({ success: true, result: number })
 })
 
 app.get(`${RANDOM_ROUTES}/:length`, (ctx) => {
-  const inputLength = Number.parseInt(ctx.req.param("length"))
+  const inputLength = Number.parseInt(ctx.req.param("length"), RADIX)
   const { min, max } = extractMinAndMax(ctx)
-  const numbers = Array.from(new Array(!Number.isNaN(inputLength) ? inputLength : DEFAULT_LENGTH ),()=>generateRandom(min, max))
+  const numbers = Array.from(
+    new Array(!Number.isNaN(inputLength) ? inputLength : DEFAULT_LENGTH),
+    () => generateRandom(min, max),
+  )
 
-  return ctx.json({success: true, result: numbers})
+  return ctx.json({ success: true, result: numbers })
 })
 
-
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running`)
-})
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is running`)
+  },
+)
